6279567cae72fef0565e698d5464d7819db1f7a8
Didier Caron <DidierCaron@pvh.com>
2018-08-09 20:45:45 +0200

    enable path to be sourced from the body similar to the id.

--- src/main/java/com/couchbase/connect/kafka/sink/SubDocumentWriter.java
+++ src/main/java/com/couchbase/connect/kafka/sink/SubDocumentWriter.java
@@ -1,23 +1,55 @@
 package com.couchbase.connect.kafka.sink;
 
+import com.couchbase.client.deps.io.netty.buffer.ByteBuf;
 import com.couchbase.client.java.AsyncBucket;
 import com.couchbase.client.java.PersistTo;
 import com.couchbase.client.java.ReplicateTo;
-import com.couchbase.client.java.document.JsonDocument;
 import com.couchbase.client.java.document.json.JsonObject;
-import com.couchbase.client.java.error.DocumentDoesNotExistException;
 import com.couchbase.client.java.subdoc.AsyncMutateInBuilder;
 import com.couchbase.client.java.subdoc.SubdocOptionsBuilder;
+import com.couchbase.connect.kafka.util.DocumentPathExtractor;
 import com.couchbase.connect.kafka.util.JsonBinaryDocument;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import rx.Completable;
 
-import rx.functions.Action1;
+import java.io.IOException;
 
 import static com.couchbase.client.deps.io.netty.util.CharsetUtil.UTF_8;
 
 public class SubDocumentWriter {
+
+    private class SubdocOperation
+    {
+        private String id;
+        private String path;
+        private JsonObject data;
+
+        public String getId(){
+            return id;
+        }
+
+        public void setId(String id){
+            this.id = id;
+        }
+
+        public String getPath(){
+            return path;
+        }
+
+        public void setPath(String path){
+            this.path = path;
+        }
+
+        public JsonObject getData() {
+            return data;
+        }
+
+        public void setData(ByteBuf data){
+            this.data = JsonObject.fromJson(data.toString(UTF_8));
+
+        }
+    }
 
     private static final Logger LOGGER = LoggerFactory.getLogger(SubDocumentWriter.class);
 
@@ -27,12 +59,15 @@ public class SubDocumentWriter {
 
     private boolean createPaths;
 
+    private boolean extractPath;
+
     private boolean createDocuments;
 
-    public SubDocumentWriter(SubDocumentMode mode, String path, boolean createPaths, boolean createDocuments) {
+    public SubDocumentWriter(SubDocumentMode mode, String path, boolean extractPath, boolean createPaths, boolean createDocuments) {
 
         this.mode = mode;
         this.path = path;
+        this.extractPath = extractPath;
         this.createPaths = createPaths;
         this.createDocuments = createDocuments;
     }
@@ -45,52 +80,52 @@ public class SubDocumentWriter {
             return Completable.complete();
         }
 
-        JsonObject node = JsonObject.fromJson(document.content().toString(UTF_8));
+        SubdocOperation operation = getOperation(document);
 
         SubdocOptionsBuilder options = new SubdocOptionsBuilder().createPath(createPaths);
 
         AsyncMutateInBuilder mutation = bucket
                 .mutateIn(document.id());
 
-        if (document.content() == null && !document.id().isEmpty()) {
-            mutation = mutation.remove(path, options);
+        if (operation.data == null && !document.id().isEmpty()) {
+            mutation = mutation.remove(operation.path, options);
         }
         else {
             switch (mode) {
                 case UPSERT: {
-                    mutation = mutation.upsert(path, node, options);
+                    mutation = mutation.upsert(operation.getPath(), operation.getData(), options);
                     break;
                 }
                 case ARRAY_INSERT: {
-                    mutation = mutation.arrayInsert(path, node, options);
+                    mutation = mutation.arrayInsert(operation.getPath(), operation.getData(), options);
                     break;
                 }
                 case ARRAY_APPEND: {
-                    mutation = mutation.arrayAppend(path, node, options);
+                    mutation = mutation.arrayAppend(operation.getPath(), operation.getData(), options);
 
                     break;
                 }
                 case ARRAY_PREPEND: {
-                    mutation = mutation.arrayPrepend(path, node, options);
+                    mutation = mutation.arrayPrepend(operation.getPath(), operation.getData(), options);
 
                     break;
                 }
                 case ARRAY_INSERT_ALL: {
-                    mutation = mutation.arrayInsertAll(path, node, options);
+                    mutation = mutation.arrayInsertAll(operation.getPath(), operation.getData(), options);
 
                     break;
                 }
                 case ARRAY_APPEND_ALL: {
-                    mutation = mutation.arrayAppendAll(path, node, options);
+                    mutation = mutation.arrayAppendAll(operation.getPath(), operation.getData(), options);
 
                     break;
                 }
                 case ARRAY_PREPEND_ALL: {
-                    mutation = mutation.arrayPrependAll(path, node, options);
+                    mutation = mutation.arrayPrependAll(operation.getPath(), operation.getData(), options);
                     break;
                 }
                 case ARRAY_ADD_UNIQUE: {
-                    mutation = mutation.arrayAddUnique(path, node, options);
+                    mutation = mutation.arrayAddUnique(operation.getPath(), operation.getData(), options);
                     break;
                 }
             }
@@ -102,5 +137,30 @@ public class SubDocumentWriter {
                 .toCompletable();
 
     }
-}
 
+    private SubdocOperation getOperation(JsonBinaryDocument doc)
+    {
+        SubdocOperation subDocument = new SubdocOperation();
+        subDocument.setId(doc.id());
+
+        ByteBuf data = null;
+        if(extractPath) {
+            DocumentPathExtractor extractor = new DocumentPathExtractor(path,true);
+            try {
+                DocumentPathExtractor.DocumentExtraction extraction = extractor.extractDocumentPath(doc.content().array());
+                subDocument.setPath(extraction.getPathValue());
+                subDocument.setData(extraction.getData());
+            } catch (IOException e) {
+                LOGGER.error(e.getMessage(), e);
+            } catch (DocumentPathExtractor.DocumentPathNotFoundException e) {
+                LOGGER.error(e.getMessage(), e);
+            }
+        }
+        else {
+            subDocument.setPath(path);
+            subDocument.setData(doc.content());
+        }
+
+        return subDocument;
+    }
+}
